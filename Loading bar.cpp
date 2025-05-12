b01bfa9952d987c86d7fbc9524a666c606dcc6a8
ibrarullah23 <ibrarullah0023@gmail.com>
2025-05-12 15:55:24 +0500

    init push

---
+++ Loading bar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h> // for Sleep()
+using namespace std;
+int main()
+{
+	//  this will print     Loading Bar 
+	system("mode con: cols=100 lines=25");
+	char x = 219;	
+	cout<<"\n\n\n\n\n\n\n\n\t\t"<<"\t\t\t  L O A D I N G ...\n\n\t\t\t";
+	for(int i=0 ; i<50 ; i++){
+		if(i<21)
+		Sleep(1);
+		if(i==4 || i==21 )
+		Sleep(150); // <windows.h>
+		else if (i==10 || i==50 || i==35)
+		Sleep(800);
+		cout<<x;
+		if(i==49){
+		cout<<"\n\n\t\t\t\t\t    D O N E ...";
+		}
+	}
+	cout<<"\n\n\n\n\n\n\n\n";
+	////////
+	system("pause");
+	return 0;
+}
